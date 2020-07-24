78a38427768f382b907a0c4e733c853d1e938517
stel4enkk <stel4enkk@iMac-stel4enkk.local>
2020-07-24 16:26:08 +0300

    capcha

--- 3/BillCounter.m
+++ 3/BillCounter.m
@@ -4,7 +4,19 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSNumber *mySum = [[NSNumber alloc] initWithInt:0];
+    for (int i =0; i< [bill count]; i++){
+        if (i!=index){
+            mySum = [NSNumber numberWithInt:([mySum intValue]+ [bill[i] intValue])];
+        }
+    }
+    if ([mySum intValue]/2 == [sum intValue]){
+    return @"Bon Appetit";
+    }
+NSNumber *correctSum = [[NSNumber alloc] initWithInt:0];
+correctSum = [NSNumber numberWithInt:([sum intValue] -[mySum intValue]/2)];
+NSLog(@"%i", [correctSum intValue]);
+return [correctSum stringValue];
 }
 
 @end
