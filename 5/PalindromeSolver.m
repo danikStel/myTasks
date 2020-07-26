7d5689a20190680cf2a003460ce3b59b76b55aaa
stel4enkk <stel4enkk@iMac-stel4enkk.local>
2020-07-27 02:26:17 +0300

    hh

--- 5/PalindromeSolver.m
+++ 5/PalindromeSolver.m
@@ -4,7 +4,42 @@
 
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    if ([n intValue]%2!=0){
+        return @"-1";
+    }
+    NSMutableArray *pol = [[NSMutableArray alloc] initWithCapacity:[s length]];
+    for (int i=0; i < [s length]; i++) {
+        NSString *ichar  = [NSString stringWithFormat:@"%c", [s characterAtIndex:i]];
+        [pol addObject:ichar];
+    }
+    int len = [n intValue];
+    NSString *max = pol[0];
+    for (int i = 0 ; i<len ;i++){
+        if ([pol[i] intValue] > [max intValue]){
+            max = pol[i];
+        }
+    }
+    for (NSUInteger stPos=0, endPos=len-1; stPos < len/2; stPos++, endPos--) {
+       if(pol[stPos]!=pol[endPos] && pol[stPos]!=max && pol[endPos]!=max){
+            pol[stPos]=max;
+            pol[endPos]=max;
+            continue;
+        }
+        if (pol[stPos]==pol[endPos]){
+         continue;
+        }
+      if ([pol[stPos] intValue]>[pol[endPos]intValue]){
+          pol[endPos]= pol[stPos];
+          continue;
+        }
+        if ([pol[stPos]intValue]<[pol[endPos]intValue] ){
+            pol[stPos] = pol[endPos];
+            continue;
+        }
+    }
+    NSString * result = [[pol valueForKey:@"description"] componentsJoinedByString:@""];
+    NSLog(@"%@",result);
+    return result;
 }
 
 @end
